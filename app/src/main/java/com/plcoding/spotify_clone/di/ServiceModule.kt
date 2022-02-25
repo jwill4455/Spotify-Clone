2762a1ccd8cdf07154332ad1185dccd03d0e78bb
Burak <burakceylann@outlook.com>
2022-02-25 17:00:24 +0300

    Add service module

---
+++ app/src/main/java/com/plcoding/spotify_clone/di/ServiceModule.kt
@@ -0,0 +1,10 @@
+package com.plcoding.spotify_clone.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+}
